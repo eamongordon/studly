0504aa7c9c162d5ad9171bfeb7af6f350c941295
yuyeonk1 <yuyeonk11@gmail.com>
2025-09-13 13:57:01 -0400

    update floating icons to move better

--- components/ui/floatingIcons.tsx
+++ components/ui/floatingIcons.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-type IconPos = { top?: string; bottom?: string; left?: string; right?: string; slow?: boolean };
+type IconPos = { top?: string; bottom?: string; left?: string; right?: string };
 
 const ICONS: IconPos[] = [
-  { top: "80px", left: "72px", slow: true },
+  { top: "80px", left: "72px" },
   { top: "120px", right: "88px" },
   { top: "160px", left: "32%" },
   { top: "200px", right: "34%" },
@@ -18,19 +18,19 @@ export default function FloatingIcons() {
   return (
     <div className="absolute inset-0 pointer-events-none z-0">
       {ICONS.map((p, i) => (
-        <img
+        <div
           key={i}
-          src="/lightbulb.svg"
-          alt="lightbulb"
-          className={`absolute w-6 h-6 opacity-50 ${p.slow ? "bg-float-slow" : "bg-float"}`}
-          style={{
-            top: p.top,
-            bottom: p.bottom,
-            left: p.left,
-            right: p.right,
-            animationDelay: `${i * 0.4}s`,
-          }}
-        />
+          className="absolute float-sync"
+          style={{ top: p.top, bottom: p.bottom, left: p.left, right: p.right }}
+        >
+          <div className="spin-sync">
+            <img
+              src="/lightbulb.svg"
+              alt="lightbulb"
+              className="w-6 h-6 opacity-50 wobble-sync"
+            />
+          </div>
+        </div>
       ))}
     </div>
   );
